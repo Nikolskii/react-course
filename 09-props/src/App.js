f834d0dfa7374ef26a46cbff8e4e040c1f1b7d00
Denis Nikolskiy <den-apple@ya.ru>
2022-09-19 10:51:23 +0200

    09-props

--- 09-props/src/App.js
+++ 09-props/src/App.js
@@ -1,7 +1,13 @@
 import './App.css';
+import PetInfo from './components/PetInfo';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <PetInfo animal="cat" age={7} />
+      <PetInfo animal="dog" age={5} />
+    </div>
+  );
 }
 
 export default App;
