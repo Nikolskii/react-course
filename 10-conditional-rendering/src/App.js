b2d720af7020f037636387bcdba92b3760249b6c
Denis Nikolskiy <den-apple@ya.ru>
2022-09-19 12:27:10 +0200

    10-conditional-rendering

--- 10-conditional-rendering/src/App.js
+++ 10-conditional-rendering/src/App.js
@@ -4,8 +4,8 @@ import PetInfo from './components/PetInfo';
 function App() {
   return (
     <div className="App">
-      <PetInfo animal="cat" age={7} />
-      <PetInfo animal="dog" age={5} />
+      <PetInfo animal="cat" age={7} hasPet />
+      <PetInfo animal="dog" age={5} hasPet={false} />
     </div>
   );
 }
--- 10-conditional-rendering/src/components/PetInfo.js
+++ 10-conditional-rendering/src/components/PetInfo.js
@@ -1,10 +1,12 @@
 function PetInfo(props) {
-  const { animal, age } = props;
+  const { animal, age, hasPet } = props;
 
-  return (
+  return hasPet ? (
     <h1>
       My {animal} is {age} years old
     </h1>
+  ) : (
+    <h2>I don't have an animal</h2>
   );
 }
 
