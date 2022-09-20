7e4740d9a557598244cc57c9ded42e51fad38630
Denis Nikolskiy <den-apple@ya.ru>
2022-09-20 14:44:37 +0200

    16-react-fragment

--- 16-react-fragment/src/App.js
+++ 16-react-fragment/src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
+import Card from './components/Card';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Card />
+    </div>
+  );
 }
 
 export default App;
---
+++ 16-react-fragment/src/components/Card.js
@@ -0,0 +1,11 @@
+const Card = () => {
+  return (
+    <>
+      <h1>Denis</h1>
+      <h3>I am learning React</h3>
+      <button>Like!</button>
+    </>
+  );
+};
+
+export default Card;
