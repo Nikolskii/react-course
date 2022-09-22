cbb9b9b8fb70511a6a4cdaf8f61d66c3069f83bd
Denis Nikolskiy <den-apple@ya.ru>
2022-09-22 08:57:30 +0200

    19-children-prop

--- 19-children-prop/src/App.js
+++ 19-children-prop/src/App.js
@@ -1,7 +1,20 @@
 import './App.css';
+import Wrapper from './components/Wrapper';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Wrapper color="lightblue">
+        <h2>Text inside of the Wrapper</h2>
+        <button>Click me</button>
+      </Wrapper>
+      <Wrapper color="lightgreen">
+        <h2>Another text</h2>
+        <p>Some description</p>
+        <input type="text" placeholder="Enter value"></input>
+      </Wrapper>
+    </div>
+  );
 }
 
 export default App;
---
+++ 19-children-prop/src/components/Wrapper.js
@@ -0,0 +1,12 @@
+function Wrapper(props) {
+  const style = {
+    backgroundColor: props.color,
+    width: '250px',
+    padding: '20px',
+    margin: '20px auto',
+  };
+
+  return <div style={style}>{props.children}</div>;
+}
+
+export default Wrapper;
