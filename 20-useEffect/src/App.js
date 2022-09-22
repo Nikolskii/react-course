63cfc9e9f7029717011c74c756cbf4045fd5c9cb
Denis Nikolskiy <den-apple@ya.ru>
2022-09-22 10:20:08 +0200

    20-useEffect

--- 20-useEffect/src/App.js
+++ 20-useEffect/src/App.js
@@ -1,7 +1,19 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  return <div className="App"></div>;
+  const [todo, setTodo] = useState(null);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos/3')
+      .then((response) => response.json())
+      .then((json) => setTodo(json));
+  }, []);
+
+  console.log('App rendered');
+  console.log(todo);
+
+  return <div className="App">{todo && <h1>{todo.title}</h1>}</div>;
 }
 
 export default App;
