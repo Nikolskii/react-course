47975a2f0e19ddecbc7adbf3c9c3551efa0c9dbc
Denis Nikolskiy <den-apple@ya.ru>
2022-09-26 11:16:19 +0200

    24-todo-app-v1

--- 24-todo-app-v1/src/App.js
+++ 24-todo-app-v1/src/App.js
@@ -10,11 +10,15 @@ function App() {
     setTodos([...todos, text]);
   };
 
+  const deleteTodoHandler = (index) => {
+    setTodos(todos.filter((_, idx) => idx !== index));
+  };
+
   return (
     <div className="App">
       <h1>Todo App</h1>
       <TodoForm addTodo={addTodoHandler} />
-      <TodoList todos={todos} />
+      <TodoList todos={todos} deleteTodo={deleteTodoHandler} />
     </div>
   );
 }
--- 24-todo-app-v1/src/components/Todos/Todo.js
+++ 24-todo-app-v1/src/components/Todos/Todo.js
@@ -1,5 +1,13 @@
-function Todo({ todo }) {
-  return <h3>{todo}</h3>;
+import { RiTodoFill } from 'react-icons/ri';
+import styles from './Todo.module.css';
+
+function Todo({ todo, index, deleteTodo }) {
+  return (
+    <div className={styles.todo} onDoubleClick={() => deleteTodo(index)}>
+      <RiTodoFill className={styles.todoIcon} />
+      <div className={styles.todoText}>{todo}</div>
+    </div>
+  );
 }
 
 export default Todo;
