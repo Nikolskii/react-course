43341ed8d237eb1868bdd8933fbbfa18c27fa37f
Denis Nikolskiy <den-apple@ya.ru>
2022-10-25 20:44:03 +0200

    26-react-router

---
+++ 26-react-router/src/App.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Contacts from './components/Contacts';
+import NotFound from './components/NotFound';
+import './App.css';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="contacts" element={<Contacts />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
