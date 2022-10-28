bcc1d48aa8fad1a99174cdb564023d8b4f7cc5fb
Denis Nikolskiy <den-apple@ya.ru>
2022-10-28 22:00:18 +0200

    26-react-router

--- 26-react-router/src/components/About.jsx
+++ 26-react-router/src/components/About.jsx
@@ -1,5 +1,3 @@
-import { Link } from 'react-router-dom';
-
 const About = () => {
   return <h1>About</h1>;
 };
--- 26-react-router/src/components/Courses.jsx
+++ 26-react-router/src/components/Courses.jsx
@@ -1,11 +1,44 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import queryString from 'query-string';
 import courses from '../data/courses';
 
+const SORT_KEYS = ['title', 'slug', 'id'];
+
+function sortCourses(courses, key) {
+  const sortedCourses = [...courses];
+
+  if (!key || !SORT_KEYS.includes(key)) {
+    return sortedCourses;
+  }
+
+  sortedCourses.sort((a, b) => (a[key] > b[key] ? 1 : -1));
+
+  return sortedCourses;
+}
+
 const Courses = () => {
+  const location = useLocation();
+  const query = queryString.parse(location.search);
+  const navigate = useNavigate();
+  const [sortKey, setSortKey] = useState(query.sort);
+
+  const [sortedCourses, setSortedCourses] = useState(
+    sortCourses(courses, sortKey)
+  );
+
+  useEffect(() => {
+    if (!SORT_KEYS.includes(sortKey)) {
+      navigate('.');
+      setSortKey();
+      setSortedCourses([...courses]);
+    }
+  }, [sortKey, navigate]);
+
   return (
     <>
-      <h1>Courses</h1>
-      {courses.map((course) => (
+      <h1>{sortKey ? `Courses sorted by ${sortKey}` : 'Courses'}</h1>
+      {sortedCourses.map((course) => (
         <div key={course.id}>
           <Link to={course.slug} className="courseLink">
             {course.title}
