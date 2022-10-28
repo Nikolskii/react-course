8075dbe610f8d55a0953dce81df7c16acdfca2ea
Denis Nikolskiy <den-apple@ya.ru>
2022-10-28 18:40:27 +0200

    26-react-router

--- 26-react-router/src/components/SingleCourse.jsx
+++ 26-react-router/src/components/SingleCourse.jsx
@@ -1,16 +1,25 @@
-import { Link, useParams } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 import courses from '../data/courses';
 
 const SingleCourse = () => {
   const params = useParams();
 
+  const navigate = useNavigate();
+
   const course = courses.find((course) => course.slug === params.courseSlug);
+
+  useEffect(() => {
+    if (!course) {
+      navigate('..', { relative: 'path' });
+    }
+  }, [course, navigate]);
 
   return (
     <>
-      <h1>{course.title}</h1>
-      <h2>{course.slug}</h2>
-      <h3>{course.id}</h3>
+      <h1>{course?.title}</h1>
+      <h2>{course?.slug}</h2>
+      <h3>{course?.id}</h3>
       <Link to=".." relative="path">
         All courses
       </Link>
