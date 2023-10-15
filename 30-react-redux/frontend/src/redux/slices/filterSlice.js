a85ac731d1063292fbb9e75facac4163eab733ad
Denis Nikolsky <den-apple@ya.ru>
2023-10-15 10:32:11 +0200

    30-react-redux

---
+++ 30-react-redux/frontend/src/redux/slices/filterSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  title: '',
+};
+
+const filterSlice = createSlice({
+  name: 'filter',
+  initialState,
+  reducers: {
+    setTitleFilter: (state, action) => {
+      return { ...state, title: action.payload };
+    },
+  },
+});
+
+export const { setTitleFilter } = filterSlice.actions;
+
+export default filterSlice.reducer;
--- 30-react-redux/frontend/src/redux/store.js
+++ 30-react-redux/frontend/src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import booksReducer from './books/reducer';
+import filterReducer from './slices/filterSlice';
 
 const store = configureStore({
   reducer: {
     books: booksReducer,
+    filter: filterReducer,
   },
 });
 
